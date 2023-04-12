3dc151ef5c49184b4056a15e37b83320c067d4da
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2023-04-12 19:53:30 +0300

    fix

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -7,3 +7,29 @@ $(function () {
     return false;
   });
 });
+
+$(document).ready(function () {
+  // Додати плавний скролінг до всіх посилань
+  $('a').on('click', function (event) {
+    // Перевірка, чи має якір
+    if (this.hash !== '') {
+      // Зупинка стандартної поведінки посилання
+      event.preventDefault();
+
+      // Збереження якоря
+      var hash = this.hash;
+
+      // Використання методу animate для додавання плавного скролінгу до якоря
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Додавання якоря в URL після скролінгу
+          window.location.hash = hash;
+        }
+      );
+    } // Кінець перевірки
+  });
+});
